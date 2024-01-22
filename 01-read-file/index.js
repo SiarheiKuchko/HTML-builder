ba768272c7c7656275f82acd97e31e81a4cececc
Siarhei Kuchko <siarheikuchko@gmail.com>
2024-01-22 21:50:30 +0200

    feat: update code

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,20 +1,10 @@
 const fs = require('fs');
 
-const stream = new fs.ReadStream('./01-read-file/text.txt', 'utf8');
+const readStream = new fs.createReadStream(__dirname + '/text.txt', 'utf8');
 
-stream.on('readable', function () {
-  let data = stream.read();
-  console.log(data);
-});
-
-stream.on('end', function () {
-  console.log('THE END');
-});
-
-stream.on('error', function (err) {
-  if (err.code === 'ENOENT') {
-    console.log('Файл не найден');
-  } else {
-    console.error(err);
+readStream.on('readable', function () {
+  let data = readStream.read();
+  if (data !== null) {
+    console.log(data);
   }
 });
