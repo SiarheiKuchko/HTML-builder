c0d83a4ec2e4ff77e45a95e57de505ecea2fcdc7
Siarhei Kuchko <siarheikuchko@gmail.com>
2024-01-22 23:19:19 +0200

    feat: add implementation for task 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+process.stdout.write('Enter any text:\n');
+
+process.stdin.on('data', function (data) {
+  const writeData = data.toString().trim();
+  if (writeData === 'exit') {
+    process.exit();
+  }
+  fs.appendFile(__dirname + '/text.txt', writeData, (err) => {
+    if (err) throw err;
+  });
+});
